f803a8df508dcbdf16011da17f473d66fbbd64e4
David FRANCOIS <david.francois@webflows.fr>
2011-05-27 14:40:31 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BitcoinPool::Application.config.secret_token = 'a4ae24dc63098f2a6d4b84766fa03e7332653638464024d89445fca72b062c0233334606c0625021d84ae688292ce299a9dd7307ea9a28a542196a47b8315d73'
